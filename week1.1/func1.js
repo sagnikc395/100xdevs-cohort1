0befac69a97b20e0583a295bfb283c85744a3146
sagnikc395 <sagnikchatterjee395@gmail.com>
2023-11-12 16:10:05 +0530

    src: error handling with noop

--- week1.1/func1.js
+++ week1.1/func1.js
@@ -10,10 +10,25 @@ function multiply(index1, index2) {
 function divide(index1, index2) {
   return index1 / index2;
 }
-function doArithmatic(firstEl, secondEl, arithmaticFn) {
+function doArithmatic(firstEl, secondEl, whatToDo) {
   //the third arguemnt is the function itself
-  return arithmaticFn(firstEl, secondEl);
+  if (whatToDo === "sum") {
+    return sum(firstEl, secondEl);
+  } else if (whatToDo === "subtract") {
+    return subtract(firstEl, secondEl);
+  } else if (whatToDo === "multiply") {
+    return multiply(firstEl, secondEl);
+  } else if (whatToDo === "divide") {
+    return divide(firstEl, secondEl);
+  } else {
+    throw new Error("noop!");
+  }
 }
-
-let answer = doArithmatic(12, 45, sum);
-console.log(answer);
+try {
+  let answer = doArithmatic(12, 45, sum);
+  console.log(answer);
+  let ans2 = doArithmatic(123, 123123, gilgamesh);
+  console.log(ans2);
+} catch (err) {
+  console.error(err);
+}
