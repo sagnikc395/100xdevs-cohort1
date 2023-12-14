594cf5f38207c7fef68ed961e72472efa0007cbb
sagnikc395 <sagnikchatterjee395@gmail.com>
2023-12-15 00:38:43 +0530

    src: generate using jwt

--- week3/week3.4/index.js
+++ week3/week3.4/index.js
@@ -5,9 +5,16 @@ const jwt = require("jsonwebtoken");
 const secret = "supersecret";
 
 let ans = jwt.sign("sagnik321@gmail.com", secret);
+let obj = jwt.sign(
+  {
+    username: "sagnik321@gmail.com",
+    password: "superS3cr3t",
+  },
+  secret
+);
 
 console.log(ans);
-
+console.log(obj);
 //convert this string back
 
 let res = jwt.verify(ans, secret, (err, val) => {
@@ -18,3 +25,11 @@ let res = jwt.verify(ans, secret, (err, val) => {
 });
 
 console.log(res);
+
+let res2 = jwt.verify(obj, secret, (err, res) => {
+  if (err) {
+    console.error(err);
+  }
+  const { username, pass } = res;
+  console.log(`Original object was ${{ username, pass }}`);
+});
