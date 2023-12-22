48f6d24d914ed19336f148f8928841bc49210f31
sagnikc395 <sagnikchatterjee395@gmail.com>
2023-12-22 09:03:13 +0530

    refactor: remove center tag

--- week5/courseappv1/src/Signup.jsx
+++ week5/courseappv1/src/Signup.jsx
@@ -6,42 +6,48 @@ import { Typography } from "@mui/material";
 function Signup() {
   return (
     <>
-      <center>
-        <div style={{ paddingTop: 150, marginBottom: 10 }}>
-          <Typography variant="h6">Welcome to Coursera! SignUp Below</Typography>
-        </div>
-      </center>
-      <center>
-        <Card
+      <div
+        style={{
+          paddingTop: 150,
+          marginBottom: 10,
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <Typography variant="h6">Welcome to Coursera! SignUp Below</Typography>
+      </div>
+      <Card
+        variant="outlined"
+        style={{
+          width: 400,
+          padding: 20,
+
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <TextField
+          id="username"
+          type="email"
+          label="Email"
           variant="outlined"
-          style={{
-            width: 400,
-            padding: 20,
-          }}
-        >
-          <TextField
-            id="username"
-            type="email"
-            label="Email"
-            variant="outlined"
-            fullWidth={true}
-          />
-          <br />
-          <br />
-          <TextField
-            id="password"
-            type="password"
-            label="Password"
-            variant="outlined"
-            fullWidth={true}
-          />
-          <br />
-          <br />
-          <Button size="large" variant="contained">
-            SignUp
-          </Button>
-        </Card>
-      </center>
+          fullWidth={true}
+        />
+        <br />
+        <br />
+        <TextField
+          id="password"
+          type="password"
+          label="Password"
+          variant="outlined"
+          fullWidth={true}
+        />
+        <br />
+        <br />
+        <Button size="large" variant="contained">
+          SignUp
+        </Button>
+      </Card>
     </>
   );
 }
