bc96c0b697afa48339afa92a571c34e3551f5926
sagnikc395 <sagnikchatterjee395@gmail.com>
2023-12-22 23:34:27 +0530

    todo: fix more components and add changes

--- week5/courseappv1/src/App.jsx
+++ week5/courseappv1/src/App.jsx
@@ -3,6 +3,7 @@ import AppBar from "./AppBar";
 import Signin from "./Signin";
 import { Routes, Route } from "react-router-dom";
 import Signup from "./Signup";
+import AddCourse from "./AddCourse";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route path="/signin" element={<Signin />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/addcourse" element={<AddCourse />} />
         </Routes>
       </div>
     </>
