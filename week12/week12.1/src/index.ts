4016b7966bdc9fa19ce98b05448049f7440a4f90
sagnik@work <sagnikchatterjee607@gmail.com>
2024-04-02 12:34:14 +0530

    src: classes in ts

---
+++ week12/week12.1/src/index.ts
@@ -0,0 +1,22 @@
+class DateClass {
+  private timeZone: string;
+  constructor(timeZone: string) {
+    this.timeZone = timeZone;
+  }
+  //methods
+  getTime() {
+    const d = new Date();
+    return d.getTime();
+  }
+
+  getMonth() {
+    return new Date().getMonth();
+  }
+
+  getCountry() {
+    return this.timeZone;
+  }
+}
+
+const dateObj = new DateClass("11 Dec 2023");
+dateObj.getTime();
