b8820caf4163d966dfddf4d7658df0bc91e5b8bb
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-04-03 07:22:29 +0530

    src: passed initial props

--- week12/week12.2/youtube-clone/src/pages/index.tsx
+++ week12/week12.2/youtube-clone/src/pages/index.tsx
@@ -6,15 +6,15 @@ import { VideoCard } from "@/components/VideoCard";
 
 export default function Home() {
   return (
-    <main>
+    <div>
       <VideoCard
         image={"/yt-thumbnail.jpeg"}
         thumbnail={"/profile-image.jpeg"}
         title={"Isekai is Dead "}
         author={"Gigguk"}
-        views={`${Math.floor(Math.random() * 1_000_000)} k views`}
-        timestamp={`${new Date().getDate()} days ago`}
+        views={`100k views`}
+        timestamp={`3 days ago`}
       />
-    </main>
+    </div>
   );
 }
