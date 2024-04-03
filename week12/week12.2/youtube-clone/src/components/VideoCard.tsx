f025e7a0e1968a0cb20b7c57f8e0955b6c0e9646
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-04-03 07:19:50 +0530

    src: passed some sample props

--- week12/week12.2/youtube-clone/src/components/VideoCard.tsx
+++ week12/week12.2/youtube-clone/src/components/VideoCard.tsx
@@ -1,19 +1,21 @@
-export function VideoCard() {
+export function VideoCard(props: any) {
   return (
     <div>
-      <img src="/yt-thumbnail.jpeg" className="rounded-xl" />
+      <img src={props.image} className="rounded-2xl" />
       <div className="grid grid-cols-12 pt-2">
         <div className="col-span-1">
           <img
             className="rounded-full w-20 h-20 bg-slate-600"
-            src="/profile-image.jpeg"
+            src={props.thumbnail}
           />
         </div>
         <div className="col-span-11 pl-5">
-          <div className="">No More Isekai 😌</div>
-          <div className="col-span-11  text-gray-600 text-base">Gigguk</div>
+          <div className="">{props.title}</div>
           <div className="col-span-11  text-gray-600 text-base">
-            2 Mn | 13 days ago
+            {props.author}
+          </div>
+          <div className="col-span-11  text-gray-600 text-base">
+            {props.views} | {props.timestamp}
           </div>
         </div>
       </div>
--- week12/week12.2/youtube-clone/src/pages/index.tsx
+++ week12/week12.2/youtube-clone/src/pages/index.tsx
@@ -7,7 +7,14 @@ import { VideoCard } from "@/components/VideoCard";
 export default function Home() {
   return (
     <main>
-      <VideoCard />
+      <VideoCard
+        image={"/yt-thumbnail.jpeg"}
+        thumbnail={"/profile-image.jpeg"}
+        title={"Isekai is Dead "}
+        author={"Gigguk"}
+        views={`${Math.floor(Math.random() * 1_000_000)} k views`}
+        timestamp={`${new Date().getDate()} days ago`}
+      />
     </main>
   );
 }
