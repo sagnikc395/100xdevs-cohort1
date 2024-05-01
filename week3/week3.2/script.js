ef4ee72a0fb448056cc051e24d1beb497f7f9966
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-05-01 09:42:02 +0530

    ui updat

--- week3/week3.2/script.js
+++ week3/week3.2/script.js
@@ -42,3 +42,21 @@ window.setInterval(() => {
     },
   ]);
 }, 5000);
+
+function parsedResponse(data) {
+  console.log(data);
+}
+
+function callback(resp) {
+  resp.json().then(parsedResponse);
+}
+
+function onPress() {
+  fetch("http://localhost:3000/todos", {
+    method: "POST",
+    body: JSON.stringify({
+      title: "gym",
+      description: "go to gym at 7",
+    }),
+  });
+}
