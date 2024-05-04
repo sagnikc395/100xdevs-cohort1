6374baec87da0c14c51d57ace8c2edcbd90a3eec
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-05-05 00:37:01 +0530

    src: basic routes descp

--- week3/week-3-assignment/index.js
+++ week3/week-3-assignment/index.js
@@ -1,4 +1,43 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 app.useExpress.json();
+
+//routes
+
+app.post("/admin/courses", (req, res) => {
+  //logic to create a course
+});
+
+app.put("/admin/courses/:courseId", (req, res) => {
+  //logic to edit a course
+});
+
+app.get("/admin/courses", (req, res) => {
+  //logic to get all courses
+});
+
+//user routes
+app.post("/users/signup", (req, res) => {
+  //logic to sign up a user
+});
+
+app.post("/users/login", (req, res) => {
+  //logic to log in user
+});
+
+app.get("/users/courses", (req, res) => {
+  //logic to test all courses
+});
+
+app.post("/users/courses/:courseId", (req, res) => {
+  //logic to purchase a course
+});
+
+app.get("/users/purchasedCourse", (req, res) => {
+  //logic to view the purchases courses
+});
+
+app.listen(3000, () => {
+  console.log(`Server is listening on port 3000`);
+});
