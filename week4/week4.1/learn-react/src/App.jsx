2eee4f616e751760112cb3b2388f0ff1437ccddf
sagnik@work <sagnikchatterjee607@gmail.com>
2024-05-13 13:26:26 +0530

    src: react making the state cahnges interactively, instead of being very explicit

--- week4/week4.1/learn-react/src/App.jsx
+++ week4/week4.1/learn-react/src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// const todos = [
+//   {
+//     title: "go to gym",
+//     description: "go to gym from home",
+//     id: 1,
+//   },
+//   {
+//     title: "go to home",
+//     description: "go to home from gym",
+//     id: 2,
+//   },
+// ];
+
+import { useState } from "react";
+
+const todo = {
+  title: "go to gym",
+  description: "go to gym from home",
+  id: 1,
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todo, setTodo] = useState({
+    title: "init",
+    description: "this starts the action",
+    id: 0,
+  });
+
+  setInterval(() => {
+    setTodo({
+      title: "go eat something",
+      description: " eat food",
+      id: Math.random() * 100,
+    });
+  }, 5000);
 
   return (
     <>
+      <h1>Hi there</h1>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {todo.id}
+        <br />
+        {todo.description}
+        <br />
+        {todo.title}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
