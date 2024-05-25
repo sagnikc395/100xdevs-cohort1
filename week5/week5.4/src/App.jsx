892f95335efb1a5d60b0b4e5f18276b10e9bc224
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-05-26 00:26:51 +0530

    src: restriucture

--- week5/week5.4/src/App.jsx
+++ week5/week5.4/src/App.jsx
@@ -1,27 +1,83 @@
-import { useState } from "react";
-import "./App.css";
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import { createContext, useState } from "react";
+import { Card, Typography, Button } from "@mui/material";
 
-function App() {
-  const [counter, setCounter] = useState(0);
+//using context api for prop drilling
+const CountContext = createContext();
 
-  const handleInc = () => {
-    setCounter(counter + 1);
-  };
-
-  const handleDec = () => {
-    setCounter(counter - 1);
-  };
+const App = () => {
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <p>Welcome to the counter game</p>
-      <div>
-        <button onClick={handleInc}>Increase Counter</button>
-        <button onClick={handleDec}>Decreaase Counter </button>
-        <div>Current Value is : {counter}</div>
+    //similar to us doing useState but helps in providing only things that are required 
+    // useState for the global level.
+    
+    <CountContext.Provider
+      value={{
+        count: count,
+        setCount: setCount,
+      }}
+    >
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <Card style={{ padding: 20, width: 500 }}>
+          <Typography>welcome to the counter game</Typography>
+          <br />
+          <Buttons count={count} setCount={setCount} />
+          <CountComponent count={count} />
+        </Card>
       </div>
-    </>
+    </CountContext.Provider>
   );
-}
+};
+
+const Buttons = ({ count, setCount }) => {
+  return (
+    <div style={{ display: "flex", justifyContent: "space-between" }}>
+      <Increase count={count} setCount={setCount} />
+      <Decrease count={count} setCount={setCount} />
+    </div>
+  );
+};
+
+const Increase = ({ count, setCount }) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+      }}
+    >
+      <Button
+        variant="container"
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Increase
+      </Button>
+    </div>
+  );
+};
+
+const Decrease = ({ count, setCount }) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+      }}
+    >
+      <Button
+        variant="container"
+        onClick={() => {
+          setCount(count - 1);
+        }}
+      >
+        Decrease
+      </Button>
+    </div>
+  );
+};
 
 export default App;
