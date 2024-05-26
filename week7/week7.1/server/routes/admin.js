212ceda3432060f45f6cca20cd9460c16b397991
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-05-26 11:37:34 +0530

    src: added the admin route

--- week7/week7.1/server/routes/admin.js
+++ week7/week7.1/server/routes/admin.js
@@ -21,8 +21,78 @@ router.get("/me", authenticateJWT, async (req, res) => {
   });
 });
 
-
 //post route for new admin signup
-router.post('/signup',(req,res) => {
-    
-})
+router.post("/signup", (req, res) => {
+  const { username, password } = req.body;
+  const findAdmin = (admin) => {
+    if (admin) {
+      res.status(403).json({
+        message: "Admin already exists !",
+      });
+    } else {
+      const obj = { username: username, password: password };
+      const newAdmin = new Admin(obj);
+      newAdmin.save();
+
+      const token = jwt.sign(
+        {
+          username,
+          role: "admin",
+        },
+        SECRET,
+        {
+          expiresIn: "1h",
+        }
+      );
+      res.json({
+        message: "Admin created successfully!",
+        token,
+      });
+    }
+  };
+  Admin.findOne({ username }).then(findAdmin);
+});
+
+//login
+router.post("/login", async (req, res) => {
+  const { username, password } = req.headers;
+  const admin = await Admin.findOne({ username, password });
+  if (admin) {
+    const token = jwt.sign({ username, role: "admin" }, SECRET, {
+      expiresIn: "1h",
+    });
+    res.json({ message: "Logged in successfully", token });
+  } else {
+    res.status(403).json({ message: "Invalid username or password" });
+  }
+});
+
+router.post("/courses", authenticateJWT, async (req, res) => {
+  const course = new Course(req.body);
+  await course.save();
+  res.json({ message: "Course created successfully", courseId: course.id });
+});
+
+router.put("/courses/:courseId", authenticateJWT, async (req, res) => {
+  const course = await Course.findByIdAndUpdate(req.params.courseId, req.body, {
+    new: true,
+  });
+  if (course) {
+    res.json({ message: "Course updated successfully" });
+  } else {
+    res.status(404).json({ message: "Course not found" });
+  }
+});
+
+router.get("/courses", authenticateJWT, async (req, res) => {
+  const courses = await Course.find({});
+  res.json({ courses });
+});
+
+router.get("/course/:courseId", authenticateJWT, async (req, res) => {
+  const courseId = req.params.courseId;
+  const course = await Course.findById(courseId);
+  res.json({ course });
+});
+
+export { router };
